059db3caf472b2b1fc3b86bc3ab20bd211d18111
valter lafuente junior <actct.vlafuente@bv.com.br>
2023-09-25 16:43:21 -0300

    Cocriando jogo forca.py e jogos.py

---
+++ jogos.py
@@ -0,0 +1,8 @@
+print("*********************************")
+print("******Escolha o seu jogo!********")
+print("*********************************")
+
+print("(1) Forca  (2) Adivinhação")
+
+jogo = int(input("Qual jogo você quer jogar?"))
+

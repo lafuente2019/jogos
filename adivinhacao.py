6fe813f886ec7cc819a5899eaee9df49c1546fef
valter lafuente junior <actct.vlafuente@bv.com.br>
2023-09-26 15:58:14 -0300

    modularizando adivinhaÃcao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,8 +5,6 @@ def jogar():
     
     imprime_welcome()
     numero_secreto = captura_numero_secreto()
-    
-    total_de_tentativas = 0
     pontos = 1000
     nivel = nivel_jogo()
 
@@ -18,7 +16,6 @@ def jogar():
         chute_str = input("Digite um número entre 1 e 100: ")
         print("Você digitou ", chute_str)
         chute = int(chute_str)
-        
 
         if(chute < 1 or chute > 100):
             print("Você deve digitar um número entre 1 e 100!")
@@ -45,8 +42,6 @@ def jogar():
                     print("O número secreto era {}. Você fez {}".format(
                         numero_secreto, pontos))
 
-    print("Fim do jogo")
-
 
 def imprime_welcome():
     print("*********************************")
@@ -64,17 +59,19 @@ def nivel_jogo():
     return nivel
 
 def total_tentativas(nivel):
-    while(nivel != 1 or nivel != 2 or nivel !=3):
-        if(nivel == 1):
-            total_de_tentativas = 20
-            return total_de_tentativas
-        elif(nivel == 2):
-            total_de_tentativas = 10
-            return total_de_tentativas
-        elif(nivel == 3):
-            total_de_tentativas = 5
-            return total_de_tentativas
+    if(nivel == 1):
+        total_de_tentativas = 20
+        return total_de_tentativas
+    elif(nivel == 2):
+        total_de_tentativas = 10
+        return total_de_tentativas
+    elif(nivel == 3):
+        total_de_tentativas = 5
+        return total_de_tentativas
+        
+
 
 if(__name__ == "__main__"):
     jogar()    
+
 

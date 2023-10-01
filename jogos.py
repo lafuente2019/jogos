bec1fddad2725861acf0b721c8f7fa5561f63402
valter lafuente junior <actct.vlafuente@bv.com.br>
2023-10-01 20:06:36 -0300

    AlMelhoria do codigo no arquivo jogo.py

--- jogos.py
+++ jogos.py
@@ -1,26 +1,31 @@
 import forca
 import adivinhacao
 
-count = 0
-while(count == 0):
-    print()
-    print("*********************************")
-    print("******Escolha o seu jogo!********")
-    print("*********************************")
-    print()
+def escolher_jogo():
+    count = 0
+    while(count == 0):
+        print()
+        print("*********************************")
+        print("******Escolha o seu jogo!********")
+        print("*********************************")
+        print()
 
-    print("(1) Forca  (2) Adivinhação  (0) Sair" )
+        print("(1) Forca  (2) Adivinhação  (0) Sair" )
 
-    jogo = int(input("Qual jogo você quer jogar?"))
-    print()
+        jogo = int(input("Qual jogo você quer jogar?"))
+        print()
 
 
-    if(jogo == 1):
-        forca.jogar()
-    elif(jogo == 2):
-        adivinhacao.jogar()
-    elif(jogo > 2):
-        print("### Opção invalida ###")
-    elif(jogo == 0):
-        count +=1    
+        if(jogo == 1):
+            forca.jogar()
+        elif(jogo == 2):
+            adivinhacao.jogar()
+        elif(jogo > 2):
+            print("### Opção invalida ###")
+        elif(jogo == 0):
+            count +=1
+
+if(__name__ == "__main__"):
+    escolher_jogo()
+
 
